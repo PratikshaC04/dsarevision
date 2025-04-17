28205412160af9a129b4cff453c69e2180f80116
PratikshaC04 <chougulepratiksha23@gmail.com>
2025-04-17 18:12:21 +0530

    Seacrh ib binarysearch tree

---
+++ searchinbst.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while (root != NULL && root->val != val) {
+            if (root->val > val)
+                root = root->left;
+            else
+                root = root->right;
+        }
+        return root;
+    }
+};
+
