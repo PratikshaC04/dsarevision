593bc617dbd20c3f33017dd4d8d3e23af6bfaabb
PratikshaC04 <chougulepratiksha23@gmail.com>
2025-04-25 18:19:39 +0530

    Added code to check for valid parenthesis using stack

---
+++ validparenthesis.cpp
@@ -0,0 +1,29 @@
+#include <stack>
+#include <string>
+
+class Solution {
+public:
+    bool isValid(std::string s) {
+        std::stack<char> stk;
+
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stk.push(ch);
+            } else {
+                if (stk.empty()) return false;
+
+                char top = stk.top();
+                stk.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stk.empty();
+    }
+};
+
