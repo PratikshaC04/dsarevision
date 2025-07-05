8f4412f91476add123f98255dbe6e5be85f20fad
PratikshaC04 <chougulepratiksha23@gmail.com>
2025-07-05 20:09:24 +0530

    Selection sort

--- selection_sort1.cpp
+++ selection_sort1.cpp
@@ -1,10 +1,18 @@
-void selection_sort{
-    int i,j;
-   for(i=0;i<n-1;i++)
-   {
-    for(j=0;j<n-i-1;j++)
-    {
-if(a[j])
+void selection_sort(int arr[], int n) {
+    int i, j, mini;
+
+    for (i = 0; i < n - 1; i++) {
+        mini = i;  // Assume the minimum is at the current index
+
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[mini]) {
+                mini = j;  // Update index of the minimum element
+            }
+        }
+
+        // Swap the found minimum element with the element at index i
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
     }
-   }
 }
