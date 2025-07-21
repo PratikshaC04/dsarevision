cf7cbaf12406651adadeebcded4f32c621c9358e
pratiksha@410 <chougulepratiksha23@gmail.com>
2025-07-21 08:08:48 +0530

    Chnaged ans

--- numofplatforms.cpp
+++ numofplatforms.cpp
@@ -1,29 +1,24 @@
-#include<bits/stdc++.h>
- using namespace std;
- 
  int countPlatforms(int n,int arr[],int dep[])
  {
-    int ans=1; //final value
-    for(int i=0;i<=n-1;i++)
+    sort(arr,arr+n);
+    sort(dep,dep+n);
+ 
+    int ans=1;
+    int count=1;
+    int i=1,j=0;
+    while(i<n && j<n)
     {
-        int count=1; // count of overlapping interval of only this   iteration
-        for(int j=i+1;j<=n-1;j++)
+        if(arr[i]<=dep[j]) //one more platform needed
         {
-            if((arr[i]>=arr[j] && arr[i]<=dep[j]) ||
-           (arr[j]>=arr[i] && arr[j]<=dep[i]))
-           {
-               count++;
-           }
+            count++;
+            i++;
         }
-        ans=max(ans,count); //updating the value
+        else //one platform can be reduced
+        {
+            count--;
+            j++;
+        }
+        ans=max(ans,count); //updating the value with the current maximum
     }
     return ans;
- }
- 
- int main()
- {
-    int arr[]={900,945,955,1100,1500,1800};
-    int dep[]={920,1200,1130,1150,1900,2000};
-    int n=sizeof(dep)/sizeof(dep[0]);
-    cout<<"Minimum number of Platforms required "<<countPlatforms(n,arr,dep)<<endl;
  }
