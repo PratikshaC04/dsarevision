acea9f257d670f12187a591f368fa0df5ca54f08
pratiksha@410 <chougulepratiksha23@gmail.com>
2025-07-25 17:38:09 +0530

    wild acrd match until dp que

---
+++ wildcardmatch.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isAllStars(string &S1, int i) {
+        for (int j = 0; j <= i; j++) {
+            if (S1[j] != '*') return false;
+        }
+        return true;
+    }
+
+    bool wildcardMatchingUtil(string &S1, string &S2, int i, int j, vector<vector<int>> &dp) {
+        // Base Cases
+        if (i < 0 && j < 0) return true;
+        if (i < 0 && j >= 0) return false;
+        if (j < 0 && i >= 0) return isAllStars(S1, i);
+
+        // Memoization check
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (S1[i] == S2[j] || S1[i] == '?')
+            return dp[i][j] = wildcardMatchingUtil(S1, S2, i - 1, j - 1, dp);
+
+        if (S1[i] == '*')
+            return dp[i][j] = wildcardMatchingUtil(S1, S2, i - 1, j, dp) || wildcardMatchingUtil(S1, S2, i, j - 1, dp);
+
+        return dp[i][j] = false;
+    }
+
+    bool isMatch(string s, string p) {
+        int n = p.size();
+        int m = s.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return wildcardMatchingUtil(p, s, n - 1, m - 1, dp);
+    }
+};
